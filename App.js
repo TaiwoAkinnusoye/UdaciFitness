e5d34441ad008d6e4f09ccc19ca063951f1d63ed
taiwo <taiwo@kudabank.com>
2020-06-25 10:11:51 +0100

    created slider component

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
+  handlePress = () => {
+    // alert('Hello');
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -18,5 +21,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff',
   },
 });
