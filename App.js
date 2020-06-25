5698a16cbaf6c9ba31972eb90c99234f405e8f12
taiwo <taiwo@kudabank.com>
2020-06-25 09:18:23 +0100

    created add workout entry component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
@@ -8,7 +7,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <AddEntry />
-        <Ionicons name="ios-pizza" color="blue" size={100} />
       </View>
     );
   }
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,80 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
+import UdaciSlider from './UdaciSlider';
+import UdaciSteppers from './UdaciSteppers';
+import DateHeader from './DateHeader';
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] + step;
+
+      return {
+        ...this.state,
+        [metric]: count > max ? max : count,
+      };
+    });
+  };
+
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] - step;
+
+      return {
+        ...this.state,
+        [metric]: count < 0 ? 0 : count,
+      };
+    });
+  };
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      ...this.state,
+      [metric]: value,
+    }));
+  };
   render() {
-    return <View>{getMetricMetaInfo('eat').getIcon()}</View>;
+    const metaInfo = getMetricMetaInfo();
+    return (
+      <View>
+        <DateHeader date={new Date().toLocaleDateString()} />
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider' ? (
+                <UdaciSlider
+                  value={value}
+                  onChange={() => this.slide(key, value)}
+                  {...rest}
+                />
+              ) : (
+                <UdaciSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />
+              )}
+            </View>
+          );
+        })}
+      </View>
+    );
   }
 }
