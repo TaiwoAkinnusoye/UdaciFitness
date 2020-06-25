3493e1b0d65b608eb1d0f11684bac7d3d70ab20e
taiwo <taiwo@kudabank.com>
2020-06-25 11:57:03 +0100

    added async storage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -72,6 +73,7 @@ export default class AddEntry extends Component {
     // navigate to home
 
     // save to 'DB'
+    submitEntry({ key, entry });
 
     // clear local notification
   };
@@ -83,6 +85,7 @@ export default class AddEntry extends Component {
     //navigate to home
 
     // update "DB"
+    removeEntry(key);
   };
   render() {
     const metaInfo = getMetricMetaInfo();
